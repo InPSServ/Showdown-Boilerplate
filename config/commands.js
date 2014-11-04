82625ba22e73f0a644c482c6cdb390acb673615b
InPSServ <allenidicula@gmail.com>
2014-11-03 20:00:54 -0500

    addeddate

--- config/commands.js
+++ config/commands.js
@@ -665,7 +665,7 @@ peppa: function(target, room, user) {
 										'</font><i><font color=purple><b>"I spread my asscheeks for Shrek" 8/16/2014 </b></font></i></font><br />' +
 										'</font><i><font color=purple><b>"Where\'s Picopie? What!? He\'s jacking off!?" 8/16/2014 </b></font></i></font><br />'	+
 										'</font><i><font color=purple><b>"Why is it so long!?" 9/10/2014 </b></font></i></font><br />' + 
-										'<img src="http://107.161.19.92:8000/TCs/peppa2.jpg/">'
+										'<img src="http://107.161.19.92:8000/TCs/peppa2.jpg/"> 11/3/2014'
                                       )
             },
 	  giant: function(target, room, user) {
